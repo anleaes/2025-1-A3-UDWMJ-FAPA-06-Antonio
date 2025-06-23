a03d2dbc015b8ddddef205908505d877aba9900c
xande <xandedbh@outlook.com>
2025-06-23 16:01:25 -0300

    melhorias

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from ndia.models import Ndia, ItemNdia
 
 # Create your views here.
 
 def home(request):
-    template_name ='core/home.html'
-    context = {}
-    return render(request, template_name, context)
+    ndia = Ndia.objects.all()
+    ndia_items = ItemNdia.objects.all()
+    context = {
+        'ndia': ndia,
+        'ndia_items': ndia_items,
+    }
+    return render(request, 'core/home.html', context)
